76e6d80d0d0671595fa431b5e16fcabf2520194b
Stephen Tvedt <stephen@stephentvedt.com>
2017-08-23 21:28:07 -0400

    add static asset files for client css and js

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+(function(){
+
+  function init(){
+    someFunction();
+  }
+
+  var config;
+
+  // all things DOM
+  var $pageTitle = document.getElementById('page-title');
+
+  function someFunction(){
+    console.log('hi from client js');
+  }
+
+})();
