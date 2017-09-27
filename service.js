d6df3a7d5385c5063dcc05db48837a9a0db6a781
Stephen Tvedt <stephen@stephentvedt.com>
2017-09-27 09:59:49 -0400

    getting view to at least render. socket is next for updating/creating endpoint

--- service.js
+++ service.js
@@ -3,13 +3,10 @@ var sessionUser = new User;
 
 function logInUser(user){
   User.findOne({ username: user.username }, function (err, doc){
-    console.log('found user',doc);
-
     if (doc === null ){
       //create new user
-      var currentUser = new User (
-        user
-      );
+      user.currentScore = { wins: 0, losses: 0};
+      var currentUser = new User ( user );
       currentUser.save(function (err) {
         if (err) {
           console.log(err);
@@ -24,5 +21,34 @@ function logInUser(user){
     }
   });
 }
-var Service = { logInUser };
+
+function getUser(user){
+  console.log('getUser()');
+  User.findOne({ username: user.username }, function (err, doc){
+      return doc;
+    }
+  );
+}
+
+function updateScoreWin(channel){
+  User.findOne({ username: channel }, function (err, doc){
+    console.log('update score win');
+    //create new user
+    doc.currentScore.wins++;
+    doc.markModified('currentScore.wins');
+    doc.save();
+  });
+}
+
+function updateScoreLoss(channel){
+  User.findOne({ username: channel }, function (err, doc){
+    console.log('update score loss');
+    //create new user
+    doc.currentScore.losses++;
+    doc.markModified('currentScore.losses');
+    doc.save();
+  });
+}
+
+var Service = { logInUser, getUser, updateScoreWin, updateScoreLoss };
 module.exports = Service;
